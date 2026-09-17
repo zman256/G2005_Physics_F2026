66b424f45092fce8b62f05efc0087bc1706dffd5
zman256 <45179184+zman256@users.noreply.github.com>
2026-09-17 12:25:55 -0400

    Merged With Joss

--- game/src/main.cpp
+++ game/src/main.cpp
@@ -11,68 +11,37 @@ See documentation here: https://www.raylib.com/, and examples here: https://www.
 int screenWidth = 1200;
 int screenHeight = 800;
 
-const unsigned int TARGET_FPS = 60;
+const unsigned int TARGET_FPS = 50;
 float time = 0;
 Vector2 birdPosition;
 
+
 int main()
 {
-    InitWindow(screenWidth, screenHeight, "Massan");
+    InitWindow(screenWidth, screenHeight, "Physics-1");
     SetTargetFPS(TARGET_FPS);
-    birdPosition = {30.0f, (float)(screenHeight)};
-    float circleRadius = 40.0f;
-    Vector2 circlePosition = { (float)screenWidth / 2, (float)screenHeight / 2 };
-    Vector2 circleSpeed = { 5.0f, 4.0f };
-    
+    birdPosition = { 30, (float)(screenHeight - 50) };
 
     while (!WindowShouldClose())
     {
-        //bouncing circle
-        circlePosition.x += circleSpeed.x;
-        circlePosition.y += circleSpeed.y;
-
-        if ((circlePosition.x >= (screenWidth - circleRadius)) || (circlePosition.x <= circleRadius))
-        {
-            circleSpeed.x *= -1.0f;
-        }
-        if ((circlePosition.y >= (screenHeight - circleRadius)) || (circlePosition.y <= circleRadius))
-        {
-            circleSpeed.y *= -1.0f;
-        }
-
-        // 3D CAMERA
-        Camera3D camera = {0};
-        camera.position = (Vector3) { 0.0f, 10.0f, 10.0f};
-        camera.target = (Vector3) { 0.0f, 0.0f, 0.0f };
-        camera.up = (Vector3) { 0.0f, 1.0f, 0.0f };
-        camera.fovy = 50.0f;
-        camera.projection = CAMERA_PERSPECTIVE;
-
-        Vector3 cubePosition = { 0.0f, 0.0f, 0.0f };
-
         BeginDrawing();
             ClearBackground(WHITE);
             DrawText("Hello world!", 10, 10, 20, LIGHTGRAY);
-            DrawCircleV(birdPosition, 30.0f, RED);
-            //Bouncy Ball
-            DrawCircleV(circlePosition, circleRadius, BLUE); 
 
-            // Draw circle that follows the mouse
+
+            DrawCircleV(birdPosition, 30, RED);
+
+
+            //Draw circle that follows the mouse
             Vector2 mousePos = GetMousePosition();
-            DrawCircleLines(mousePos.x, mousePos.y, 30, BLACK);
+            DrawCircleLines(mousePos.x, mousePos.y, 50, DARKBROWN);
+
+            //Thick lines
+            DrawLineEx(Vector2{ 0.0f, 700.0f }, Vector2{ 1200, 700 }, 5, DARKGREEN);
 
             time += 1;
 
             GuiSliderBar(Rectangle{ 60, 5, 1000, 10 }, "Time", TextFormat("%.2f", time), &time, 0, 240);
-
-            BeginMode3D(camera);
-                
-            DrawCube(cubePosition, 2.0f, 2.0f, 2.0f, RED);
-            DrawCubeWires(cubePosition, 2.0f, 2.0f, 2.0f, MAROON);
-
-            DrawGrid(10, 1.0f);
-
-            EndMode3D();
 
 
         EndDrawing();
